7cca4aa447792699b6b41a4ac5f744af940c358a
Nataly Nerovnya <natanerovnja@gmail.com>
2016-09-08 09:59:18 +0300

    make header partial

--- Day3/DarkAndLightSide/Models/User.cs
+++ Day3/DarkAndLightSide/Models/User.cs
@@ -4,7 +4,7 @@ namespace DarkAndLightSide.Models
 {
     public class User
     {
-        public string Name { get; set; }
+        public string Name {  get; set; }
         public int Age { get; set; }
         public Side Side { get; set; }
     }
