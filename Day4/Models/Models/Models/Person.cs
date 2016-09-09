036544d94b6f8999f486006677d4f96c04bd7342
Nataly Nerovnya <natanerovnja@gmail.com>
2016-09-09 12:41:22 +0300

    Work in class

---
+++ Day4/Models/Models/Models/Person.cs
@@ -0,0 +1,36 @@
+﻿using Models.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Models.Models
+{
+	public class Person
+	{
+		public int PersonId { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public DateTime BirthDate { get; set; }
+		public Address HomeAddress { get; set; }
+		public bool IsActive { get; set; }
+		public Role Role { get; set; }
+	}
+    //[ModelBinder(typeof(CustomModelBinder))] -- чтобы не регестрирвоать в глобал асаксе
+	public class Address
+	{
+		public string Line1 { get; set; }
+		public string Line2 { get; set; }
+		public string City { get; set; }
+		public string PostalCode { get; set; }
+		public string Country { get; set; }
+	}
+
+	public enum Role
+	{
+		Admin,
+		User,
+		Guest
+	}
+}
